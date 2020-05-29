491d312ed9f328dcef4850d162e794e5249f964f
progrunner17 <g.sho1500@gmail.com>
2020-05-30 02:29:50 +0900

    add debug echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,17 @@ set -e
 address=$(curl -s ifconfig.me)
 echo $1
 echo $1 | wc
-echo ${INPUT_XREA_ACCOUNT}
-echo ${INPUT_XREA_ACCOUNT} | wc
 echo ${XREA_ACCOUNT}
 echo ${XREA_ACCOUNT} | wc
+echo INPUT_XREA_ACCOUNT
+echo ${INPUT_XREA_ACCOUNT}
+echo ${INPUT_XREA_ACCOUNT} | wc
+echo INPUT_XREA_SERVER
+echo ${INPUT_XREA_SERVER}
+echo ${INPUT_XREA_SERVER} | wc
+echo INPUT_XREA_API_KEY
+echo ${INPUT_XREA_API_KEY}
+echo ${INPUT_XREA_API_KEY} | wc
 resp=$(curl -sS -X POST \
   -d "account=${INPUT_XREA_ACCOUNT}" \
   -d "server_name=${INPUT_XREA_SERVER}" \
