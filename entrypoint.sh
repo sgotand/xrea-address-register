618d39e8e9b3f7787815ec24240d1d5a899ebf8d
progrunner17 <g.sho1500@gmail.com>
2020-05-30 02:33:38 +0900

    fix variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 address=$(curl -s ifconfig.me)
+echo  ${address}
 echo $1
 echo $1 | wc
 echo ${XREA_ACCOUNT}
@@ -19,7 +20,7 @@ echo ${INPUT_XREA_API_KEY} | wc
 resp=$(curl -sS -X POST \
   -d "account=${INPUT_XREA_ACCOUNT}" \
   -d "server_name=${INPUT_XREA_SERVER}" \
-  -d "api_secret_key=${INPUT_XREA_KEY}" \
+  -d "api_secret_key=${INPUT_XREA_API_KEY}" \
   -d "param[addr]=${address}" \
   https://api.xrea.com/v1/tool/ssh_ip_allow)
 
